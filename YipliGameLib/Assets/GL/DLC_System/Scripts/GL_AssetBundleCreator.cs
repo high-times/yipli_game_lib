3c2a7b0cf253085e6e320cccc50fe08e020e53d1
Suraj Jorwekar <vismay.patel1103@gmail.com>
2022-06-22 16:37:41 +0530

    DLC_System part 1
    Scene DLC is implemented

---
+++ YipliGameLib/Assets/GL/DLC_System/Scripts/GL_AssetBundleCreator.cs
@@ -0,0 +1,27 @@
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Yipli.GameLib.DlcSystem
+{
+    public class GL_AssetBundleCreator : Editor
+    {
+        [MenuItem("Assets/GL_DLC/Make All Windows DLC Bundles")]
+        static void MakeAllWindowsBundleFiles()
+        {
+            BuildPipeline.BuildAssetBundles("Assets/GL/AB/Resources/Windows", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        }
+
+        [MenuItem("Assets/GL_DLC/Make All Android DLC Bundles")]
+        static void MakeAllAndroidBundleFiles()
+        {
+            BuildPipeline.BuildAssetBundles("Assets/GL/AB/Resources/Android", BuildAssetBundleOptions.None, BuildTarget.Android);
+        }
+
+        [MenuItem("Assets/GL_DLC/Make All iOS DLC Bundles")]
+        static void MakeAlliOSBundleFiles()
+        {
+            BuildPipeline.BuildAssetBundles("Assets/GL/AB/Resources/iOS", BuildAssetBundleOptions.None, BuildTarget.iOS);
+        }
+    }
+}
+#endif
---
+++ YipliGameLib/Assets/GL/DLC_System/Scripts/GL_DLCSceneManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+namespace Yipli.GameLib.DlcSystem
+{
+    public class GL_DLCSceneManager : MonoBehaviour
+    {
+        // private variables
+        private string sceneDownloadUrl = "file:///Users/yipli-m1/FW/UP/yipli_game_lib/YipliGameLib/Assets/GL/AB/Resources/Android/scene_ygl";
+        private string sceneInBundle = "";
+
+        // assets variables
+        AssetBundle sceneBundle;
+
+        // Unity operations
+        private void Start()
+        {
+            StartCoroutine(DownloadAllScenes());
+        }
+
+        // Dlc operations
+        private IEnumerator DownloadAllScenes()
+        {
+            UnityWebRequest scenesDLCrequest = UnityWebRequestAssetBundle.GetAssetBundle(sceneDownloadUrl);
+
+            yield return scenesDLCrequest.SendWebRequest();
+            if (scenesDLCrequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError($"Scene Download failed : {scenesDLCrequest.error}");
+                yield break;
+            }
+
+            sceneBundle = DownloadHandlerAssetBundle.GetContent(scenesDLCrequest);
+            string[] scenePaths = sceneBundle.GetAllScenePaths();
+
+            foreach (string path in scenePaths)
+            {
+                sceneInBundle = Path.GetFileNameWithoutExtension(path);
+            }
+
+            if (sceneInBundle.Length <= 2) yield break;
+
+            SceneManager.LoadScene(sceneInBundle);
+        }
+    }
+}
